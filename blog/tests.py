f76c31b916bd87e493bdbade3352c01a7062bcb0
TenJeong <33208278+TenJeong@users.noreply.github.com>
2023-04-22 19:43:49 +0900

    Add test code for post list page

--- blog/tests.py
+++ blog/tests.py
@@ -48,3 +48,37 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         # '아직 게시물이 없습니다.' doesn't appear no longer
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+    def test_post_detail(self):
+        # there is a post
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world.',
+        )
+        # the post's url is '/blog/1'
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # test for the first post detail page
+        # load page successfully
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # the same navbar with post list page exists
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        # the web browser tab's title has the first post's title
+        self.assertIn(post_001.title, soup.title.text)
+
+        # the first post's title is in post area
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+
+        # the author of the first post is in post area (To be implemented)
+        # TBI
+
+        # the content of the first post is in post area
+        self.assertIn(post_001.content, post_area.text)
