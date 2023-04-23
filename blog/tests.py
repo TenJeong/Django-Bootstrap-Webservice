98adf7cf31fca570a92fe7e0a07db3972a29968c
TenJeong <33208278+TenJeong@users.noreply.github.com>
2023-04-23 23:36:51 +0900

    Modulize navbar and footer

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         # get post list page
         response = self.client.get('/blog/')
@@ -15,39 +32,7 @@ class TestView(TestCase):
         # the title is named 'Blog'
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        # navbar exists
-        navbar = soup.nav
-        # 'Blog', 'About Me' is in navbar
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
-
-        # if there is no single post,
-        self.assertEqual(Post.objects.count(), 0)
-        # '아직 게시물이 없습니다' appears in main area
-        main_area = soup.find('div', id='main-area')
-        self.assertIn('아직 게시물이 없습니다', main_area.text)
-
-        # if there are two posts,
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.',
-            content='Hello World. We are the world.',
-        )
-        post_002 = Post.objects.create(
-            title='두 번째 포스트입니다.',
-            content='1등이 전부는 아니잖아요?',
-        )
-        self.assertEqual(Post.objects.count(), 2)
-
-        # page refreshed
-        response = self.client.get('/blog/')
-        soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(response.status_code, 200)
-        # there are two posts in main area
-        main_area = soup.find('div', id='main-area')
-        self.assertIn(post_001.title, main_area.text)
-        self.assertIn(post_002.title, main_area.text)
-        # '아직 게시물이 없습니다.' doesn't appear no longer
-        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+        self.navbar_test(soup)
 
     def test_post_detail(self):
         # there is a post
@@ -64,21 +49,4 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # the same navbar with post list page exists
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
-
-        # the web browser tab's title has the first post's title
-        self.assertIn(post_001.title, soup.title.text)
-
-        # the first post's title is in post area
-        main_area = soup.find('div', id='main-area')
-        post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
-
-        # the author of the first post is in post area (To be implemented)
-        # TBI
-
-        # the content of the first post is in post area
-        self.assertIn(post_001.content, post_area.text)
+        self.navbar_test(soup)
